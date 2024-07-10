07131deb703839169de20a6c9a5b02929d529034
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-10 17:44:32 -0300

    feat: add open effect to Modal

--- src/app/_components/Modal.tsx
+++ src/app/_components/Modal.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { X } from "lucide-react";
+import { motion, Variants } from "framer-motion";
 
 interface ModalProps {
   isVisible: boolean;
@@ -7,6 +10,11 @@ interface ModalProps {
   closeModal: () => void;
 }
 
+const variants: Variants = {
+  closed: { opacity: 0, y: "-100%" },
+  open: { opacity: 1, y: 0 },
+};
+
 const Modal = ({ content, isVisible, title, closeModal }: ModalProps) => {
   return (
     <div
@@ -14,14 +22,18 @@ const Modal = ({ content, isVisible, title, closeModal }: ModalProps) => {
         isVisible ? "fixed" : "hidden"
       } inset-0 flex items-center justify-center bg-black bg-opacity-90 px-4 z-50`}
     >
-      <div className="bg-[#2A2A2A] pb-5 rounded-lg shadow-lg max-w-4xl w-full relative">
+      <motion.div
+        animate={isVisible ? "open" : "closed"}
+        variants={variants}
+        className="bg-[#2A2A2A] pb-5 rounded-lg shadow-lg max-w-4xl w-full relative"
+      >
         <div className="w-full p-4 flex items-center justify-between rounded-t-lg bg-[#181818]">
           <p className="font-bold text-xl">{title || ""}</p>
           <X className="cursor-pointer" onClick={closeModal} />
         </div>
 
         <div className="px-4 pt-4">{content}</div>
-      </div>
+      </motion.div>
     </div>
   );
 };
