f21410355dabfb11049b78a82a48d379c165b56c
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-12 19:17:33 -0300

    feat: add Header animation

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,32 +1,37 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="w-full h-16 bg-[#222222] fixed top-0 left-0 z-50">
+    <motion.header
+      animate={{ top: [-200, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5, delay: 0.65 }}
+      className="opacity-0 w-full h-16 bg-gradient-to-b from-[#222222] to-[#1a1a1a] fixed top-0 left-0 rounded-b-xl shadow-md shadow-black/30 z-50 md:w-4/5 md:left-1/2 md:translate-x-[-50%]"
+    >
       <div className="container w-full mx-auto">
         <button className="w-full h-16 pr-3 flex items-center justify-end cursor-pointer md:hidden">
           <Menu />
         </button>
 
         <nav className="hidden md:block">
-          <ul className="flex h-16 w-full items-center justify-center gap-20">
+          <ul className="flex h-16 w-full items-center justify-center gap-20 text-nowrap">
             <li>
               <Link href={"#"}>Home</Link>
             </li>
             <li>
-              <Link href={"#"}>Sobre mim</Link>
+              <Link href={"#about"}>Sobre mim</Link>
             </li>
             <li>
-              <Link href={"#"}>Projetos</Link>
+              <Link href={"#projects"}>Projetos</Link>
             </li>
             <li>
-              <Link href={"#"}>Contato</Link>
+              <Link href={"#stacks"}>Tecnologias</Link>
             </li>
           </ul>
         </nav>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
