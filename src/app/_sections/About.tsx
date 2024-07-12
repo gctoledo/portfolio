eb8db7f0674ef4e5b9896a6755374eb5f76af09e
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-12 15:34:38 -0300

    feat: add intro animation to About section

--- src/app/_sections/About.tsx
+++ src/app/_sections/About.tsx
@@ -1,5 +1,6 @@
 import AboutActions from "../_components/AboutActions";
 import Avatar from "../_components/Avatar";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -7,34 +8,56 @@ const About = () => {
       className="flex flex-col gap-9 items-center text-center px-2"
       id="about"
     >
-      <Avatar />
+      <motion.div
+        animate={{ y: [1000, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="opacity-0"
+      >
+        <Avatar />
+      </motion.div>
 
-      <h1 className="text-3xl md:text-5xl font-bold leading-tight select-none">
-        Escrevendo{" "}
-        <span className="bg-gradient-to-br from-[#FF8660] to-[#9A33FF] bg-clip-text text-transparent">
-          códigos
-        </span>{" "}
-        e<br />
-        construindo{" "}
-        <span className="bg-gradient-to-tl from-[#FF8660] to-[#9A33FF] bg-clip-text text-transparent">
-          coisas
-        </span>
-        !
-      </h1>
+      <motion.div
+        className="w-full opacity-0"
+        animate={{ y: [1000, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        <h1 className="text-3xl md:text-5xl font-bold leading-tight select-none">
+          Escrevendo{" "}
+          <span className="bg-gradient-to-br from-[#FF8660] to-[#9A33FF] bg-clip-text text-transparent">
+            códigos
+          </span>{" "}
+          e<br />
+          construindo{" "}
+          <span className="bg-gradient-to-tl from-[#FF8660] to-[#9A33FF] bg-clip-text text-transparent">
+            coisas
+          </span>
+          !
+        </h1>
+      </motion.div>
 
-      <h2 className="w-full lg:w-1/3 text-[#C5C5C5] font-light text-sm">
-        Bem-vindo ao meu portfólio! Meu nome é Gabriel, sou desenvolvedor
-        full-stack com foco na stack{" "}
-        <span className="text-[#f7df1e]">JavaScript</span>/
-        <span className="text-[#007acc]">TypeScript</span>.
-        <br />
-        Sou completamente apaixonado por tecnologia e como eu consigo impactar
-        pessoas com ela.
-      </h2>
+      <motion.div
+        className="w-full lg:w-2/5 opacity-0"
+        animate={{ y: [1000, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5, delay: 0.125 }}
+      >
+        <h2 className="text-[#C5C5C5] font-light text-sm">
+          Bem-vindo ao meu portfólio! Meu nome é Gabriel, sou desenvolvedor
+          full-stack com foco na stack{" "}
+          <span className="text-[#f7df1e]">JavaScript</span>/
+          <span className="text-[#007acc]">TypeScript</span>.
+          <br />
+          Sou completamente apaixonado por tecnologia e como eu consigo impactar
+          pessoas com ela.
+        </h2>
+      </motion.div>
 
-      <div className="flex gap-4">
+      <motion.div
+        className="flex gap-4 opacity-0"
+        animate={{ y: [1000, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5, delay: 0.0625 }}
+      >
         <AboutActions />
-      </div>
+      </motion.div>
     </div>
   );
 };
