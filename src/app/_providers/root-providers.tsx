14146e335911982c6fcf1eadc3ff2ad85ca45c1d
Gabriel <gabrielcostatoledo@outlook.com>
2025-05-07 01:09:13 -0300

    refactor: remove loader

--- src/app/_providers/root-providers.tsx
+++ src/app/_providers/root-providers.tsx
@@ -1,11 +1,8 @@
-import Loader from "../_components/Loader";
 import ModalContextProvider from "../_context/ModalContext";
 
 const RootProviders = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Loader>
       <ModalContextProvider>{children}</ModalContextProvider>
-    </Loader>
   );
 };
 
